242f80393777fe754bde1f2e9d42b338aa2a4b88
fariba <fariba.darvishyan1994@gmail.com>
2020-08-23 23:48:30 +0430

    add logic tic toc toe fragment whit configuration change handel problem(in is win method):when code exist inside use direct in if check is true action but now crash I try to solve this because simple responsibly is better

--- app/src/main/java/com/example/tictactoyvs4inrow/MainActivity.java
+++ app/src/main/java/com/example/tictactoyvs4inrow/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.tictactoyvs4inrow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButtonTicTocToe;
+    private Button mButtonFourInaRow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        setListeners();
+    }
+
+    private void findViews(){
+        mButtonTicTocToe = findViewById(R.id.TicTacToeButton);
+        mButtonFourInaRow = findViewById(R.id.FourInRowButton);
+    }
+
+    private void setListeners(){
+
+        mButtonTicTocToe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentById(R.id.Container);
+
+                if (fragment == null) {
+                    TicTacToeFragment ticTacToeFragment = new TicTacToeFragment();
+                    fragmentManager
+                            .beginTransaction()
+                            .add(R.id.Container, ticTacToeFragment)
+                            .commit();
+                } else{
+                    Fragment fragment1 = fragmentManager.findFragmentById(R.id.Container);
+                    fragmentManager.beginTransaction().remove(fragment1).
+                            commit();
+
+                    TicTacToeFragment ticTacToeFragment = new TicTacToeFragment();
+                    fragmentManager.beginTransaction().add(R.id.Container, ticTacToeFragment).
+                            commit();
+                }
+            }
+        });
+
+        mButtonFourInaRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                Fragment fragment = fragmentManager.findFragmentById(R.id.Container);
+
+                if (fragment == null) {
+                    FourInRowFragment fourInRowFragment = new FourInRowFragment();
+                    fragmentManager.beginTransaction().add(R.id.Container, fourInRowFragment).
+                            commit();
+                }else{
+                    Fragment fragment2 = fragmentManager.findFragmentById(R.id.Container);
+                    fragmentManager.beginTransaction().remove(fragment2).
+                            commit();
+
+                    FourInRowFragment fourInRowFragment = new FourInRowFragment();
+                    fragmentManager.beginTransaction().add(R.id.Container, fourInRowFragment).
+                            commit();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tictactoyvs4inrow/TicTacToeFragment.java
@@ -0,0 +1,146 @@
+package com.example.tictactoyvs4inrow;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class TicTacToeFragment extends Fragment {
+
+
+    public static final String BUNDLE_IS_PLAYED = "BundleIsPlayed";
+    public static final String BUNDLE_PLAYERS_TURN = "BundlePlayersTurn";
+    private Button[] mButtons = new Button[9];
+
+    private int[] mIsChoose = {2,2,2,2,2,2,2,2};
+    private int mPlayersTurn = 0;
+
+
+    public TicTacToeFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getActivity(),"test",Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tic_tac_toe,container,false);
+        findView(view);
+        setListeners();
+
+        if (savedInstanceState!=null){
+            mPlayersTurn = savedInstanceState.getInt(BUNDLE_PLAYERS_TURN);
+            mIsChoose = savedInstanceState.getIntArray(BUNDLE_IS_PLAYED);
+            for (int i = 0; i < 9 ; i++) {
+                if (mIsChoose[i]==0) {
+                    mButtons[i].setText("+");
+                    mButtons[i].setEnabled(false);
+                } else if (mIsChoose[i]==1){
+                    mButtons[i].setText("o");
+                    mButtons[i].setEnabled(false);
+                }
+            }
+        }
+        return view;
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray(BUNDLE_IS_PLAYED, mIsChoose);
+        outState.putInt(BUNDLE_PLAYERS_TURN, mPlayersTurn);
+    }
+
+    private void findView(View view){
+
+        mButtons = new Button[]{view.findViewById(R.id.btn1),
+                                view.findViewById(R.id.btn2),
+                                view.findViewById(R.id.btn3),
+                                view.findViewById(R.id.btn4),
+                                view.findViewById(R.id.btn5),
+                                view.findViewById(R.id.btn6),
+                                view.findViewById(R.id.btn7),
+                                view.findViewById(R.id.btn8),
+                                view.findViewById(R.id.btn9)};
+
+    }
+
+    private void setListeners(){
+
+        for (int i = 0; i < 9 ; i++) {
+            final int currentIndex = i;
+            mButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mButtons[currentIndex].isEnabled()){
+
+                        if (mPlayersTurn %2==0){
+                            mButtons[currentIndex].setText("+");
+                        } else {
+                            mButtons[currentIndex].setText("o");
+                        }
+
+
+                        mIsChoose[currentIndex] = mPlayersTurn %2;
+                        mPlayersTurn++;
+                        mButtons[currentIndex].setEnabled(false);
+                        checkGameOver();
+
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void checkGameOver(){
+
+        if (isWins(0)) {
+            Snackbar.make(getActivity().findViewById(android.R.id.content), "player1 Win",
+                    Snackbar.LENGTH_LONG).show();
+
+            resetGame();
+        } else if (isWins(3)){
+            Snackbar.make(getActivity().findViewById(android.R.id.content), "player2 win",
+                    Snackbar.LENGTH_LONG).show();
+
+            resetGame();
+        }
+
+    }
+    private boolean isWins(int checkSum) {
+        if (    mIsChoose[0] + mIsChoose[1] + mIsChoose[2] == checkSum ||
+                mIsChoose[3] + mIsChoose[4] + mIsChoose[5] == checkSum ||
+                mIsChoose[6] + mIsChoose[7] + mIsChoose[8] == checkSum ||
+                mIsChoose[0] + mIsChoose[3] + mIsChoose[6] == checkSum ||
+                mIsChoose[1] + mIsChoose[4] + mIsChoose[7] == checkSum ||
+                mIsChoose[2] + mIsChoose[5] + mIsChoose[8] == checkSum ||
+                mIsChoose[0] + mIsChoose[4] + mIsChoose[8] == checkSum ||
+                mIsChoose[2] + mIsChoose[4] + mIsChoose[6] == checkSum)
+            return true;
+        else
+            return false;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 9; i++) {
+            mButtons[i].setEnabled(false);
+
+        }
+    }
+
+}
