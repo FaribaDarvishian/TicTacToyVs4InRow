885ead8773696d25eb79edb1037903e3a63499e4
fariba <fariba.darvishyan1994@gmail.com>
2020-08-25 01:22:06 +0430

    start write Four In Row Game logic findView & listeners is set but is very big code in find handel configuration change is easy (not problem) If the bottom button of the selected button is empty, it must be selected (I think solve this by Recursive  check method) I solve all as soon and add gameOver method

--- app/src/main/java/com/example/tictactoyvs4inrow/FourInRowFragment.java
+++ app/src/main/java/com/example/tictactoyvs4inrow/FourInRowFragment.java
@@ -1,64 +1,124 @@
 package com.example.tictactoyvs4inrow;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FourInRowFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.snackbar.Snackbar;
+
 public class FourInRowFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String BUNDLE_IS_PLAYED = "BundleIsPlayed";
+    public static final String BUNDLE_PLAYERS_TURN = "BundlePlayersTurn";
+    private Button[] mButtons = new Button[25];
+    private int[] mIsPlayed = new int[25];
+
+    private int playersTurn = 0;
+
 
     public FourInRowFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FourInRowFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FourInRowFragment newInstance(String param1, String param2) {
-        FourInRowFragment fragment = new FourInRowFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_four_in_row, container, false);
+        View view = inflater.inflate(R.layout.fragment_four_in_row,container,false);
+        findView(view);
+        setListeners();
+
+        if (savedInstanceState!=null){
+            playersTurn = savedInstanceState.getInt(BUNDLE_PLAYERS_TURN);
+            mIsPlayed = savedInstanceState.getIntArray(BUNDLE_IS_PLAYED);
+            for (int i = 0; i < 25 ; i++) {
+                if (mIsPlayed[i]==0) {
+                    mButtons[i].setBackgroundColor(Color.RED);
+                    mButtons[i].setEnabled(false);
+                } else if (mIsPlayed[i]==1){
+                    mButtons[i].setBackgroundColor(Color.BLUE);
+                    mButtons[i].setEnabled(false);
+                }
+            }
+        }
+        return view;
+    }
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray(BUNDLE_IS_PLAYED, mIsPlayed);
+        outState.putInt(BUNDLE_PLAYERS_TURN,playersTurn);
+    }
+
+    private void findView(View view){
+
+        mButtons = new Button[]{view.findViewById(R.id.btn0),
+                                view.findViewById(R.id.btn1),
+                                view.findViewById(R.id.btn2),
+                                view.findViewById(R.id.btn3),
+                                view.findViewById(R.id.btn4),
+                                view.findViewById(R.id.btn5),
+                                view.findViewById(R.id.btn6),
+                                view.findViewById(R.id.btn7),
+                                view.findViewById(R.id.btn8),
+                                view.findViewById(R.id.btn9),
+                                view.findViewById(R.id.btn10),
+                                view.findViewById(R.id.btn11),
+                                view.findViewById(R.id.btn12),
+                                view.findViewById(R.id.btn13),
+                                view.findViewById(R.id.btn14),
+                                view.findViewById(R.id.btn15),
+                                view.findViewById(R.id.btn16),
+                                view.findViewById(R.id.btn17),
+                                view.findViewById(R.id.btn18),
+                                view.findViewById(R.id.btn19),
+                                view.findViewById(R.id.btn20),
+                                view.findViewById(R.id.btn21),
+                                view.findViewById(R.id.btn22),
+                                view.findViewById(R.id.btn23),
+                                view.findViewById(R.id.btn24)};
+
+    }
+
+
+
+    private void setListeners(){
+
+        for (int i = 0; i < 25 ; i++) {
+            final int currentChoose = i;
+            mButtons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mButtons[currentChoose].isEnabled()){
+
+                        if (playersTurn%2==0){
+                            mButtons[currentChoose].setBackgroundColor(Color.RED);
+                        } else {
+                            mButtons[currentChoose].setBackgroundColor(Color.BLUE);
+                        }
+
+                        mIsPlayed[currentChoose] = playersTurn % 2;
+                        playersTurn++;
+                        mButtons[currentChoose].setEnabled(false);
+
+
+                    }
+                }
+            });
+        }
     }
 }
